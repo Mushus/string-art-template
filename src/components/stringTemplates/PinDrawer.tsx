c0e3aa800dffe0dbba3d0a7928a66dcce9026445
Mushus <6138982+Mushus@users.noreply.github.com>
2020-04-24 12:00:18 +0900

    fix pin number position

--- src/components/stringTemplates/PinDrawer.tsx
+++ src/components/stringTemplates/PinDrawer.tsx
@@ -9,16 +9,27 @@ interface Props {
 const PinDrawer = ({ pinPositions, drawOptions }: Props) => {
   return (
     <>
-      {pinPositions.map(([x, y], i) => (
-        <Fragment key={i}>
-          {drawOptions.withPinNumber && (
-            <text x={x + 2} y={y} fontSize={3}>
-              {i + 1}
-            </text>
-          )}
-          <circle cx={x} cy={y} r={drawOptions.pinSize} fill="black" />
-        </Fragment>
-      ))}
+      {pinPositions.map(([x, y], i) => {
+        const length = Math.sqrt(x * x + y * y);
+        const pinX = length > 0 ? x + (x / length) * 5 : 0;
+        const pinY = length > 0 ? y + (y / length) * 5 : 0;
+        return (
+          <Fragment key={i}>
+            {drawOptions.withPinNumber && (
+              <text
+                x={pinX}
+                y={pinY}
+                fontSize={3}
+                textAnchor="middle"
+                dominantBaseline="central"
+              >
+                {i}
+              </text>
+            )}
+            <circle cx={x} cy={y} r={drawOptions.pinSize} fill="black" />
+          </Fragment>
+        );
+      })}
     </>
   );
 };
