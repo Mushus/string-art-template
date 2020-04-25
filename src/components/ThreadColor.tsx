c1ee72df10465b02633f4edcec9f510839bdb9d9
Mushus <6138982+Mushus@users.noreply.github.com>
2020-04-25 22:31:56 +0900

    fix color preview

--- src/components/ThreadColor.tsx
+++ src/components/ThreadColor.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 const ThreadColor = styled.div<{ color: string }>`
   background-color: ${({ color }) => color};
+  -webkit-print-color-adjust: exact;
   display: inline-block;
   width: 0.7em;
   height: 0.7em;
